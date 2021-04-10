b12fd0f44b5a91cbb7b4b8a6ef71e26cc08beed3
TetTheDev <kirillgrayson@gmail.com>
2021-04-11 00:24:38 +0500

    Initial commit

---
+++ app/src/main/java/com/example/typego/TypingResult.java
@@ -0,0 +1,40 @@
+package com.example.typego;
+
+
+import java.util.Date;
+public class TypingResult {
+
+    private double WPM;
+    private int dictionaryType;
+    private String language;
+    private int timeInSeconds;
+    private Date completionDateTime;
+
+    public double getWPM() {
+        return WPM;
+    }
+
+    public int getDictionaryType() {
+        return dictionaryType;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public int getTimeInSeconds() {
+        return timeInSeconds;
+    }
+
+    public Date getCompletionDateTime() {
+        return completionDateTime;
+    }
+
+    public TypingResult(double WPM, int dictionaryType, String language, int timeInSeconds, Date completionDateTime) {
+        this.WPM = WPM;
+        this.dictionaryType = dictionaryType;
+        this.language = language;
+        this.timeInSeconds = timeInSeconds;
+        this.completionDateTime = completionDateTime;
+    }
+}
