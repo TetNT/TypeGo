7f9a5c700e1b7a15ed4e191a8f37b17d388011c7
TetTheDev <kirillgrayson@gmail.com>
2021-04-11 00:21:25 +0500

    Changed layout, added new buttons

--- app/src/main/java/com/example/typego/MainMenuActivity.java
+++ app/src/main/java/com/example/typego/MainMenuActivity.java
@@ -7,22 +7,31 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 public class MainMenuActivity extends AppCompatActivity {
 
+    User currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        DatabaseManager db = new DatabaseManager(this);
         Toast.makeText(this, Locale.getDefault().getDisplayLanguage(), Toast.LENGTH_LONG).show();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference userInfo = database.getReference("users");
+
 
     }
 
     public void startTesting_OnClick(View v) {
         Intent intent = new Intent (MainMenuActivity.this, TestSetupActivity.class);
+        intent.putExtra("currentUser", currentUser);
         startActivity(intent);
     }
 }
