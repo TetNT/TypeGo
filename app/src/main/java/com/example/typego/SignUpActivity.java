482cd4db7ac0a409730a0ee0a5975e8e78b06eef
TetTheDev <kirillgrayson@gmail.com>
2021-04-11 00:27:54 +0500

    Initial commit

---
+++ app/src/main/java/com/example/typego/SignUpActivity.java
@@ -0,0 +1,45 @@
+package com.example.typego;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.List;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    public void continueSignUp(View view) {
+        List<User> users;
+        FirebaseDatabase fbdb = FirebaseDatabase.getInstance();
+        DatabaseReference dbref = fbdb.getReference();
+        ValueEventListener valueEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
+                    User user = postSnapshot.getValue(User.class);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        };
+    }
+
+}
