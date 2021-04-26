d7e90d5b6abead46c99ede8c9ce7c988a9870d08
TetTheDev <kirillgrayson@gmail.com>
2021-04-26 23:38:03 +0500

    Initial commit

---
+++ app/src/main/java/com/example/typego/utils/InternetConnection.java
@@ -0,0 +1,31 @@
+package com.example.typego.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
+
+public class InternetConnection {
+    // checks if either wi-fi or cellular data is available
+    public static boolean isAvailable(Context context) {
+        boolean wifiConnected = false;
+        boolean mobileConnected = false;
+        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(context.CONNECTIVITY_SERVICE);
+        Network[] networks = cm.getAllNetworks();
+        for (Network network: networks
+             ) {
+            NetworkInfo ni = cm.getNetworkInfo(network);
+            if (ni.getTypeName().equalsIgnoreCase("wifi")) {
+                if (ni.isConnected()) wifiConnected = true;
+            }
+            if (ni.getTypeName().equalsIgnoreCase("mobile")) {
+                if (ni.isConnected()) mobileConnected = true;
+            }
+
+
+        }
+
+        return wifiConnected || mobileConnected;
+    }
+
+}
