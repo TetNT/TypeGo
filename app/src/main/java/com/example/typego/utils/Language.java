9525f88faa428985e95c18679e5deeaf5a062d7d
TetTheDev <kirillgrayson@gmail.com>
2021-04-26 23:38:11 +0500

    Initial commit

---
+++ app/src/main/java/com/example/typego/utils/Language.java
@@ -0,0 +1,45 @@
+package com.example.typego.utils;
+
+import android.content.Context;
+import android.content.res.Resources;
+
+import androidx.annotation.NonNull;
+
+import com.example.typego.R;
+
+import java.util.ArrayList;
+
+public class Language {
+    private final String identifier;
+    private final String name;
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Language(String identifier, String name) {
+        this.identifier = identifier;
+        this.name = name;
+    }
+
+     /*Returns list of languages available for testing.
+       New languages are adding here. */
+    public static ArrayList<Language> getAvailableLanguages(Context context) {
+        ArrayList<Language> languages = new ArrayList<>();
+        Resources resources = context.getResources();
+        languages.add(new Language("EN", resources.getString(R.string.english)));
+        languages.add(new Language("RU", resources.getString(R.string.russian)));
+        return languages;
+
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return name;
+    }
+}
