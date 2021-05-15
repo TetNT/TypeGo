b98e73c429de7fd6a269f36443fbb7850329cffb
TetTheDev <kirillgrayson@gmail.com>
2021-05-16 02:01:44 +0500

    Added bottom navigation handler, implemented text suggestions, user settings are now remembered. Updated spinner language selection.

--- app/src/main/java/com/example/typego/TestSetupActivity.java
+++ app/src/main/java/com/example/typego/TestSetupActivity.java
@@ -3,40 +3,66 @@ package com.example.typego;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.database.DataSetObserver;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.typego.utils.KeyConstants;
 import com.example.typego.utils.Language;
 import com.example.typego.utils.TimeConvert;
+import com.example.typego.utils.User;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.List;
 import java.util.Locale;
 
 public class TestSetupActivity extends AppCompatActivity {
-    SeekBar sb;
-    RadioGroup rbGroup;
-    RadioButton radioButton;
+    SeekBar seekBar;
+    RadioGroup rbDictionaryType;
     Spinner spinner;
     int progressInSeconds;
+    CheckBox cbTextSuggestions;
+    User currentUser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_setup);
-        sb = findViewById(R.id.seekBar);
+        currentUser = User.getFromJson(getSharedPreferences(KeyConstants.USER_STORAGE_FILE, MODE_PRIVATE).getString(KeyConstants.PREFERENCES_CURRENT_USER, null));
+        cbTextSuggestions = findViewById(R.id.cbPredictiveText);
+        rbDictionaryType = findViewById(R.id.rbDictionaryType);
+        BottomNavigationView bnv = findViewById(R.id.test_setup_bottom_nav);
+        bnv.setOnNavigationItemSelectedListener(item -> {
+            Intent intent;
+            switch (item.getItemId()) {
+                case R.id.testSetupActivity:
+                    break;
+                case R.id.accountActivity:
+                    intent = new Intent(TestSetupActivity.this, AccountActivity.class);
+                    finish();
+                    startActivity(intent);
+            }
+            return true;
+        });
+        cbTextSuggestions.setOnClickListener((v) -> {
+            if (cbTextSuggestions.isChecked())
+                Toast.makeText(this, getString(R.string.text_suggestions_enabled), Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(this, getString(R.string.text_suggestions_disabled), Toast.LENGTH_SHORT).show();
+        });
+        seekBar = findViewById(R.id.seekBar);
         TextView tvSeekbarDisplay = findViewById(R.id.tvSeekBarDisplay);
-        progressInSeconds = getSelectedSecondsOption(sb.getProgress());
+        progressInSeconds = getSelectedSecondsOption(seekBar.getProgress());
         tvSeekbarDisplay.setText(TimeConvert.convertSeconds(TestSetupActivity.this, progressInSeconds));
-        sb.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 progressInSeconds = getSelectedSecondsOption(progress);
@@ -54,19 +80,35 @@ public class TestSetupActivity extends AppCompatActivity {
             }
         });
         selectCurrentLanguageOption();
+        cbTextSuggestions.setChecked(currentUser.isPreferredTextSuggestions());
+        seekBar.setProgress(currentUser.getPreferredTimeMode());
+        ((RadioButton) rbDictionaryType.getChildAt(currentUser.getPreferredDictionaryType())).setChecked(true);
     }
+
     public void startTesting(View view) {
-        rbGroup = findViewById(R.id.rbDictionaryType);
-        radioButton = findViewById(rbGroup.getCheckedRadioButtonId());
+
+        RadioButton radioButton = findViewById(rbDictionaryType.getCheckedRadioButtonId());
         Language language = (Language)spinner.getSelectedItem();
         String languageId = language.getIdentifier();
 
-        int selectedDictionaryIndex = rbGroup.indexOfChild(radioButton);
+        int selectedDictionaryIndex = rbDictionaryType.indexOfChild(radioButton);
 
         Intent intent = new Intent(this, TypingTestActivity.class);
-        intent.putExtra("AmountOfSeconds", progressInSeconds);
-        intent.putExtra("DictionaryType", selectedDictionaryIndex);
-        intent.putExtra("DictionaryLanguageId", languageId);
+        intent.putExtra(KeyConstants.TEST_AMOUNT_OF_SECONDS, progressInSeconds);
+        intent.putExtra(KeyConstants.TEST_DICTIONARY_TYPE, selectedDictionaryIndex);
+        intent.putExtra(KeyConstants.TEST_SUGGESTIONS_ON, cbTextSuggestions.isChecked());
+        intent.putExtra(KeyConstants.TEST_DICTIONARY_LANG, languageId);
+
+        // remember user settings
+        if (currentUser != null) {
+            currentUser.setPreferredLanguage(languageId);
+            currentUser.setPreferredDictionaryType(selectedDictionaryIndex);
+            currentUser.setPreferredTextSuggestions(cbTextSuggestions.isChecked());
+            currentUser.setPreferredTimeMode(seekBar.getProgress());
+            SharedPreferences.Editor editor = getSharedPreferences(KeyConstants.USER_STORAGE_FILE, MODE_PRIVATE).edit();
+            editor.putString(KeyConstants.PREFERENCES_CURRENT_USER,User.serializeToJson(currentUser));
+            editor.apply();
+        }
         startActivity(intent);
     }
 
@@ -82,7 +124,6 @@ public class TestSetupActivity extends AppCompatActivity {
     }
 
     public void selectCurrentLanguageOption() {
-        String systemLanguage = Locale.getDefault().getDisplayLanguage().toLowerCase();
         spinner = findViewById(R.id.spinLanguageSelection);
         List<Language> languageList = Language.getAvailableLanguages(this);
         SpinnerAdapter adapter = new ArrayAdapter<>(
@@ -91,13 +132,23 @@ public class TestSetupActivity extends AppCompatActivity {
                 languageList);
         spinner.setAdapter(adapter);
         int systemLanguageIndex = 0;
+        String systemLanguage = Locale.getDefault().getDisplayLanguage().toLowerCase();
+        if (currentUser.getPreferredLanguageId()!=null) {
+            for (int i = 0; i < adapter.getCount(); i++) {
+                String languageId = ((Language) adapter.getItem(i)).getIdentifier();
+                if (languageId.equalsIgnoreCase(currentUser.getPreferredLanguageId()))
+                    systemLanguageIndex = i;
+            }
+            spinner.setSelection(systemLanguageIndex);
+            return;
+        }
         for (int i = 0; i < adapter.getCount(); i++) {
-            String language = adapter.getItem(i).toString();
+            String language = ((Language) adapter.getItem(i)).getName();
             if (language.equals(systemLanguage))
                 systemLanguageIndex = i;
         }
         spinner.setSelection(systemLanguageIndex);
-
+        return;
     }
 
 }
