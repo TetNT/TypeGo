fb40fa2f5d8b853f6c693919f9c23e10c433015d
TetTheDev <kirillgrayson@gmail.com>
2021-05-16 01:58:45 +0500

    Added emojis to WPM result, added text suggestions field, added "Start over" button, fixed result saving, added language field.

--- app/src/main/java/com/example/typego/ResultActivity.java
+++ app/src/main/java/com/example/typego/ResultActivity.java
@@ -1,20 +1,28 @@
 package com.example.typego;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.emoji.bundled.BundledEmojiCompatConfig;
+import androidx.emoji.text.EmojiCompat;
+import androidx.emoji.widget.EmojiEditText;
+import androidx.emoji.widget.EmojiTextView;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.typego.utils.Emoji;
+import com.example.typego.utils.KeyConstants;
 import com.example.typego.utils.TimeConvert;
 import com.example.typego.utils.TypingResult;
 import com.example.typego.utils.User;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-
+import java.text.DecimalFormat;
 import java.util.Calendar;
 
 public class ResultActivity extends AppCompatActivity {
@@ -23,16 +31,18 @@ public class ResultActivity extends AppCompatActivity {
     int correctWords;
     int timeInSeconds;
     int dictionaryType;
+    String dictionaryLanguageId;
     SharedPreferences spUser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EmojiCompat.Config emojiConfig = new BundledEmojiCompatConfig(this);
+        EmojiCompat.init(emojiConfig);
         setContentView(R.layout.activity_result);
-        spUser = getSharedPreferences(PreferencesManager.USER_STORAGE_FILE, MODE_PRIVATE);
-        currentUser = User.getFromJson(spUser.getString("prefCurrentUser", ""));
-        Log.d("RA user", currentUser.getUserName());
-        String dictionaryLanguageId;
-        TextView tvWPM = findViewById(R.id.tvWPM);
+        spUser = getSharedPreferences(KeyConstants.USER_STORAGE_FILE, MODE_PRIVATE);
+        currentUser = User.getFromJson(spUser.getString(KeyConstants.PREFERENCES_CURRENT_USER, null));
+        EmojiTextView tvWPM = findViewById(R.id.tvWPM);
         TextView tvPreviousResult = findViewById(R.id.tvPreviousResult);
         TextView tvBestResult = findViewById(R.id.tvBestResult);
         TextView tvCorrectWords = findViewById((R.id.tvCorrectWords));
@@ -40,6 +50,7 @@ public class ResultActivity extends AppCompatActivity {
         TextView tvDictionary = findViewById(R.id.tvDictionary);
         TextView tvAllottedTime = findViewById(R.id.tvAllottedTime);
         TextView tvLanguage = findViewById(R.id.tvLanguage);
+        TextView tvTextSuggestions = findViewById(R.id.tvTextSuggestions);
 
         tvPreviousResult.setText(getString(R.string.Previous_result) + ": " + currentUser.getLastResult());
         if (currentUser.getLastResult() == 0)
@@ -47,65 +58,64 @@ public class ResultActivity extends AppCompatActivity {
         else tvPreviousResult.setVisibility(View.VISIBLE);
 
         tvBestResult.setText(getString(R.string.Best_result) + ": " + currentUser.getBestResult());
-        if (currentUser.getBestResult()==0)
+        if (currentUser.getBestResult() == 0)
             tvBestResult.setVisibility(View.GONE);
         else tvBestResult.setVisibility(View.VISIBLE);
 
         Bundle arguments = getIntent().getExtras();
-        try {
-            correctWords = Integer.parseInt(arguments.get("correctWords").toString());
-            timeInSeconds = Integer.parseInt(arguments.get("timeInSeconds").toString());
-            int totalWords = Integer.parseInt(arguments.get("totalWords").toString());
-            dictionaryType = arguments.getInt("DictionaryType");
-            dictionaryLanguageId = arguments.getString("dictionaryLanguageId");
-            String DictionaryName;
-            if (dictionaryType == 0) DictionaryName = getString(R.string.Basic);
-            else DictionaryName = getString(R.string.Enhanced);
-            wpm = (60.0/timeInSeconds) * correctWords;
-            tvWPM.setText(wpm + " "+ getString(R.string.WordsPerMinute));
-            tvIncorrectWords.setText(getString(R.string.Incorrect_words)+ ": " + (totalWords-correctWords));
-            tvCorrectWords.setText(getString(R.string.Correct_words) + ": " + correctWords);
-            tvDictionary.setText(getString(R.string.Dictionary)+": " + DictionaryName);
-            tvAllottedTime.setText(getString(R.string.Allotted_time) + ": " + TimeConvert.convertSeconds(this, timeInSeconds));
-            tvLanguage.setText(getString(R.string.selected_language) + ": " + dictionaryLanguageId);
-            if (wpm>0) SaveResultData();
-            else Toast.makeText(this, "Результаты с 0 слов в минуту не записываются", Toast.LENGTH_SHORT).show();
+        correctWords = arguments.getInt(KeyConstants.TEST_CORRECT_WORDS);
+        timeInSeconds = arguments.getInt(KeyConstants.TEST_AMOUNT_OF_SECONDS);
+        int totalWords = arguments.getInt(KeyConstants.TOTAL_WORDS);
+        dictionaryType = arguments.getInt(KeyConstants.TEST_DICTIONARY_TYPE);
+        dictionaryLanguageId = arguments.getString(KeyConstants.TEST_DICTIONARY_LANG);
+        String textSuggestions = getString(R.string.No);
+        if (arguments.getBoolean(KeyConstants.TEST_SUGGESTIONS_ON))
+            textSuggestions = getString(R.string.Yes);
+        String DictionaryName = getString(R.string.Enhanced);
+        if (dictionaryType == 0) DictionaryName = getString(R.string.Basic);
+        wpm = (60.0 / timeInSeconds) * correctWords;
+        String wpmText = new DecimalFormat("0.#").format(wpm) + " " + getString(R.string.WordsPerMinute) + "\n" + Emoji.getEmojiOfWpm((int) wpm);
+        tvWPM.setText(wpmText);
+        tvIncorrectWords.setText(getString(R.string.Incorrect_words) + ": " + (totalWords - correctWords));
+        tvCorrectWords.setText(getString(R.string.Correct_words) + ": " + correctWords);
+        tvDictionary.setText(getString(R.string.Dictionary) + ": " + DictionaryName);
+        tvAllottedTime.setText(getString(R.string.Allotted_time) + ": " + TimeConvert.convertSeconds(this, timeInSeconds));
+        tvLanguage.setText(getString(R.string.selected_language) + ": " + dictionaryLanguageId);
+        tvTextSuggestions.setText(getString(R.string.text_suggestions) + ": " + textSuggestions);
+        if (wpm > 0) SaveResultData();
+        else
+            Toast.makeText(this, getString(R.string.msg_results_with_zero_wpm), Toast.LENGTH_SHORT).show();
 
-        } catch(Exception e) {
-            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-            Log.d("ResEx", e.getMessage());
-        }
-
+        Button bStartOver = findViewById(R.id.bStartOver);
+        bStartOver.setOnClickListener(v -> {
+            Intent intent = new Intent(ResultActivity.this, TypingTestActivity.class);
+            intent.putExtra(KeyConstants.TEST_SUGGESTIONS_ON, arguments.getBoolean(KeyConstants.TEST_SUGGESTIONS_ON));
+            intent.putExtra(KeyConstants.TEST_AMOUNT_OF_SECONDS, arguments.getInt(KeyConstants.TEST_AMOUNT_OF_SECONDS));
+            intent.putExtra(KeyConstants.TEST_DICTIONARY_TYPE, arguments.getInt(KeyConstants.TEST_DICTIONARY_TYPE));
+            intent.putExtra(KeyConstants.TEST_DICTIONARY_LANG, arguments.getString(KeyConstants.TEST_DICTIONARY_LANG));
+            finish();
+            startActivity(intent);
+        });
     }
+
 
     private void SaveResultData() {
         if (currentUser == null) {
             Toast.makeText(this, getString(R.string.saving_results_error_occurred), Toast.LENGTH_SHORT).show();
             return;
         }
+
         currentUser.setLastResult((int)wpm);
         if (currentUser.getBestResult() < currentUser.getLastResult()) {
             currentUser.setBestResult(currentUser.getLastResult());
         }
-        SharedPreferences.Editor editor = spUser.edit();
-        editor.putString("prefCurrentUser",User.serializeToJson(currentUser));
-        editor.apply();
-        if (currentUser.getUserName().equals("Guest")) {
-            // TODO: Separate TextView instead of this message:
-            Toast.makeText(this, getString(R.string.sign_in_to_save_results), Toast.LENGTH_SHORT).show();
-            return;
-        }
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference userInfo = database.getReference(currentUser.getUserName());
-
-        TypingResult result = new TypingResult(wpm, dictionaryType, "ru", timeInSeconds, Calendar.getInstance().getTime());
+        TypingResult result = new TypingResult(wpm, dictionaryType, dictionaryLanguageId, timeInSeconds, Calendar.getInstance().getTime());
         currentUser.addResult(result);
-        userInfo.setValue(currentUser);
-        //DatabaseReference userResults = userInfo.child("results");
-        //userResults.child(Calendar.getInstance().getTime().toString()).setValue(result);
-        userInfo.child("lastResult").setValue(currentUser.getLastResult());
-        userInfo.child("bestResult").setValue(currentUser.getBestResult());
+
+        SharedPreferences.Editor editor = spUser.edit();
+        editor.putString(KeyConstants.PREFERENCES_CURRENT_USER,User.serializeToJson(currentUser));
+        editor.apply();
 
 
 
@@ -115,4 +125,5 @@ public class ResultActivity extends AppCompatActivity {
     public void SaveAndContinue(View view){
         finish();
     }
+
 }
