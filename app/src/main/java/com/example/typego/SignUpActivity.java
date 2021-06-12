9d4fd12f3a024a9063f1a4ab6c53247b430beb47
TetTheDev <kirillgrayson@gmail.com>
2021-06-12 18:43:37 +0500

    Removed firebase implementation code.

--- app/src/main/java/com/example/typego/SignUpActivity.java
+++ app/src/main/java/com/example/typego/SignUpActivity.java
@@ -1,19 +1,8 @@
 package com.example.typego;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.List;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -22,27 +11,4 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
     }
-
-    public void continueSignUp(View view) {
-        List<User> users;
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference tables = database.getReference();
-        ValueEventListener valueEventListener = new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                for (DataSnapshot postSnapshot : snapshot.getChildren()) {
-                    User user = postSnapshot.getValue(User.class);
-
-
-                }
-            }
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(SignUpActivity.this, "Bruh...", Toast.LENGTH_SHORT).show();
-            }
-        };
-    }
-
 }
