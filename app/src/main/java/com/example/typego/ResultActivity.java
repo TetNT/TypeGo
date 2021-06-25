846e269f9e14d7a3dc7d019ca403ad91080ff4a4
TetTheDev <kirillgrayson@gmail.com>
2021-06-25 21:40:34 +0500

    Added more data to show. Now it checks if a user opened the activity from Account tab or from TypingTest activity.

--- app/src/main/java/com/example/typego/ResultActivity.java
+++ app/src/main/java/com/example/typego/ResultActivity.java
@@ -7,7 +7,6 @@ import androidx.emoji.widget.EmojiTextView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffColorFilter;
 import android.graphics.drawable.Drawable;
@@ -28,11 +27,16 @@ public class ResultActivity extends AppCompatActivity {
     User currentUser;
     double wpm;
     int correctWords;
+    int correctWordsWeight;
     int timeInSeconds;
     int dictionaryType;
+    int totalWords = 0;
+    boolean calledFromResultsTab;
+    boolean textSuggestionsIsOn;
     String dictionaryLanguageId;
     SharedPreferences spUser;
     TextView tvBestResult;
+    TypingResult result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,39 +56,36 @@ public class ResultActivity extends AppCompatActivity {
         TextView tvLanguage = findViewById(R.id.tvLanguage);
         TextView tvTextSuggestions = findViewById(R.id.tvTextSuggestions);
 
-        tvPreviousResult.setText(getString(R.string.Previous_result) + ": " + currentUser.getLastResult());
+        tvPreviousResult.setText(getString(R.string.previous_result) + ": " + currentUser.getLastResult());
         if (currentUser.getLastResult() == 0)
             tvPreviousResult.setVisibility(View.GONE);
         else tvPreviousResult.setVisibility(View.VISIBLE);
 
-        tvBestResult.setText(getString(R.string.Best_result) + ": " + currentUser.getBestResult());
+        tvBestResult.setText(getString(R.string.best_result) + ": " + currentUser.getBestResult());
         if (currentUser.getBestResult() == 0)
             tvBestResult.setVisibility(View.GONE);
         else tvBestResult.setVisibility(View.VISIBLE);
 
         Bundle arguments = getIntent().getExtras();
-        correctWords = arguments.getInt(KeyConstants.TEST_CORRECT_WORDS);
-        timeInSeconds = arguments.getInt(KeyConstants.TEST_AMOUNT_OF_SECONDS);
-        int totalWords = arguments.getInt(KeyConstants.TOTAL_WORDS);
-        dictionaryType = arguments.getInt(KeyConstants.TEST_DICTIONARY_TYPE);
-        dictionaryLanguageId = arguments.getString(KeyConstants.TEST_DICTIONARY_LANG);
-        String textSuggestions = getString(R.string.No);
-        if (arguments.getBoolean(KeyConstants.TEST_SUGGESTIONS_ON))
-            textSuggestions = getString(R.string.Yes);
-        String DictionaryName = getString(R.string.Enhanced);
-        if (dictionaryType == 0) DictionaryName = getString(R.string.Basic);
-        wpm = (60.0 / timeInSeconds) * correctWords;
-        String wpmText = new DecimalFormat("0.#").format(wpm) + " " + getString(R.string.WordsPerMinute) + "\n" + Emoji.getEmojiOfWpm((int) wpm);
+        initIntentData(arguments);
+        String textSuggestions = getString(R.string.no);
+        if (textSuggestionsIsOn)
+            textSuggestions = getString(R.string.yes);
+        String DictionaryName = getString(R.string.enhanced);
+        if (dictionaryType == 0) DictionaryName = getString(R.string.basic);
+        result = getCurrentResult();
+        wpm = result.getWPM();
+        String wpmText = new DecimalFormat("0.#").format(wpm) + " " + getString(R.string.wordsPerMinute) + "\n" + Emoji.getEmojiOfWpm((int) wpm);
         tvWPM.setText(wpmText);
-        tvIncorrectWords.setText(getString(R.string.Incorrect_words) + ": " + (totalWords - correctWords));
-        tvCorrectWords.setText(getString(R.string.Correct_words) + ": " + correctWords);
-        tvDictionary.setText(getString(R.string.Dictionary) + ": " + DictionaryName);
-        tvAllottedTime.setText(getString(R.string.Allotted_time) + ": " + TimeConvert.convertSeconds(this, timeInSeconds));
+        tvIncorrectWords.setText(getString(R.string.incorrect_words) + ": " + (totalWords - correctWords));
+        tvCorrectWords.setText(getString(R.string.correct_words) + ": " + correctWords);
+        tvDictionary.setText(getString(R.string.dictionary) + ": " + DictionaryName);
+        tvAllottedTime.setText(getString(R.string.allotted_time) + ": " + TimeConvert.convertSeconds(this, timeInSeconds));
         tvLanguage.setText(getString(R.string.selected_language) + ": " + dictionaryLanguageId);
         tvTextSuggestions.setText(getString(R.string.text_suggestions) + ": " + textSuggestions);
-        if (wpm > 0) SaveResultData();
-        else
+        if (wpm <= 0 && !calledFromResultsTab)
             Toast.makeText(this, getString(R.string.msg_results_with_zero_wpm), Toast.LENGTH_SHORT).show();
+        else if (wpm>0 && !calledFromResultsTab) SaveResultData();
 
         Button bStartOver = findViewById(R.id.bStartOver);
         bStartOver.setOnClickListener(v -> {
@@ -96,8 +97,23 @@ public class ResultActivity extends AppCompatActivity {
             finish();
             startActivity(intent);
         });
+        if (calledFromResultsTab) {
+            bStartOver.setVisibility(View.GONE);
+            tvPreviousResult.setVisibility(View.GONE);
+        }
+
     }
 
+    void initIntentData(Bundle arguments) {
+        correctWords = arguments.getInt(KeyConstants.TEST_CORRECT_WORDS);
+        correctWordsWeight = arguments.getInt(KeyConstants.TEST_CORRECT_WORDS_WEIGHT);
+        timeInSeconds = arguments.getInt(KeyConstants.TEST_AMOUNT_OF_SECONDS);
+        totalWords = arguments.getInt(KeyConstants.TOTAL_WORDS);
+        dictionaryType = arguments.getInt(KeyConstants.TEST_DICTIONARY_TYPE);
+        dictionaryLanguageId = arguments.getString(KeyConstants.TEST_DICTIONARY_LANG);
+        textSuggestionsIsOn = arguments.getBoolean(KeyConstants.TEST_SUGGESTIONS_ON);
+        calledFromResultsTab = arguments.getBoolean(KeyConstants.CALLED_FROM_PASSED_RESULTS);
+    }
 
     private void SaveResultData() {
         if (currentUser == null) {
@@ -108,20 +124,16 @@ public class ResultActivity extends AppCompatActivity {
         currentUser.setLastResult((int)wpm);
         if (currentUser.getBestResult() < currentUser.getLastResult()) {
             currentUser.setBestResult(currentUser.getLastResult());
-            setTextViewDrawableColor(tvBestResult, Color.argb(255, 255, 213, 0));
+            TextView tvNewBestResult = findViewById(R.id.tvNewBestResult);
+            tvNewBestResult.setVisibility(View.VISIBLE);
         }
 
-        TypingResult result = new TypingResult(wpm, dictionaryType, dictionaryLanguageId, timeInSeconds, Calendar.getInstance().getTime());
         currentUser.addResult(result);
 
         SharedPreferences.Editor editor = spUser.edit();
         editor.putString(KeyConstants.PREFERENCES_CURRENT_USER,User.serializeToJson(currentUser));
         editor.apply();
-
-
-
     }
-
 
     private void setTextViewDrawableColor(TextView textView, int color) {
         for (Drawable drawable: textView.getCompoundDrawables())
@@ -130,8 +142,23 @@ public class ResultActivity extends AppCompatActivity {
 
     public void SaveAndContinue(View view){
         finish();
-        Intent intent = new Intent(this, MainMenuActivity.class);
-        startActivity(intent);
+        if (!calledFromResultsTab) {
+            Intent intent = new Intent(this, MainMenuActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private TypingResult getCurrentResult() {
+        TypingResult result = new TypingResult(
+                correctWords,
+                correctWordsWeight,
+                totalWords,
+                dictionaryType,
+                dictionaryLanguageId,
+                timeInSeconds,
+                textSuggestionsIsOn,
+                Calendar.getInstance().getTime());
+        return result;
     }
 
 }
