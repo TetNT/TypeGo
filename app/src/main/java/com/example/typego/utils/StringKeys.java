605466b9357c95e077c987ccd9c38c5965becc24
TetTheDev <kirillgrayson@gmail.com>
2021-07-06 22:05:35 +0500

    Renamed from KeyStrings

---
+++ app/src/main/java/com/example/typego/utils/StringKeys.java
@@ -0,0 +1,24 @@
+package com.example.typego.utils;
+
+public class StringKeys {
+    // common strings
+
+    public static final String USER_STORAGE_FILE = "user_storage";
+    public static final String PREFERENCES_CURRENT_USER = "prefCurrentUser";
+    public static final String TEST_AMOUNT_OF_SECONDS = "AmountOfSeconds";
+    public static final String TEST_DICTIONARY_TYPE = "DictionaryType";
+    public static final String TEST_SUGGESTIONS_ON = "TextSuggestionsIsOn";
+    public static final String TEST_DICTIONARY_LANG = "DictionaryLanguageId";
+    public static final String TEST_CORRECT_WORDS = "correctWords";
+    public static final String TEST_CORRECT_WORDS_WEIGHT = "correctWordsWeight";
+    public static final String TOTAL_WORDS = "totalWords";
+    public static final String CALLED_FROM_PASSED_RESULTS = "calledFromPassedResults";
+    public static final String FRAGMENT_TEST_SETUP = "fragmentTestSetup";
+    public static final String FRAGMENT_ACCOUNT = "fragmentAccount";
+    public static final String FROM_MAIN_MENU = "fromMainMenu";
+
+    // language identifiers
+    public static final String LANGUAGE_ALL = "ALL";
+    public static final String LANGUAGE_RU = "RU";
+    public static final String LANGUAGE_EN = "EN";
+}
