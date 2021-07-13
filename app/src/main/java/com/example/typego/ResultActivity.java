bdb2f2b1ed291d9f493900cbe456c3865b1c680e
TetTheDev <kirillgrayson@gmail.com>
2021-07-14 00:25:56 +0500

    Now textviews' text are setting with placeholders

--- app/src/main/java/com/example/typego/ResultActivity.java
+++ app/src/main/java/com/example/typego/ResultActivity.java
@@ -7,14 +7,13 @@ import androidx.emoji.widget.EmojiTextView;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffColorFilter;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.example.typego.utils.Achievement;
 import com.example.typego.utils.Emoji;
 import com.example.typego.utils.StringKeys;
 import com.example.typego.utils.Language;
@@ -31,6 +30,7 @@ public class ResultActivity extends AppCompatActivity {
     int correctWordsWeight;
     int timeInSeconds;
     int dictionaryType;
+    int screenOrientation;
     int totalWords = 0;
     boolean calledFromResultsTab;
     boolean textSuggestionsIsOn;
@@ -56,34 +56,33 @@ public class ResultActivity extends AppCompatActivity {
         TextView tvAllottedTime = findViewById(R.id.tvAllottedTime);
         TextView tvLanguage = findViewById(R.id.tvLanguage);
         TextView tvTextSuggestions = findViewById(R.id.tvTextSuggestions);
+        TextView tvScreenOrientation = findViewById(R.id.tvScreenOrientation);
 
-        tvPreviousResult.setText(getString(R.string.previous_result) + ": " + currentUser.getLastResult());
+        tvPreviousResult.setText(getString(R.string.previous_result_pl, currentUser.getLastResult()));
         if (currentUser.getLastResult() == 0)
             tvPreviousResult.setVisibility(View.GONE);
         else tvPreviousResult.setVisibility(View.VISIBLE);
-
-        tvBestResult.setText(getString(R.string.best_result) + ": " + currentUser.getBestResult());
+        tvBestResult.setText(getString(R.string.best_result_pl, currentUser.getBestResult()));
         if (currentUser.getBestResult() == 0)
             tvBestResult.setVisibility(View.GONE);
         else tvBestResult.setVisibility(View.VISIBLE);
 
         Bundle arguments = getIntent().getExtras();
         initIntentData(arguments);
-        String textSuggestions = getString(R.string.no);
-        if (textSuggestionsIsOn)
-            textSuggestions = getString(R.string.yes);
-        String DictionaryName = getString(R.string.enhanced);
-        if (dictionaryType == 0) DictionaryName = getString(R.string.basic);
+        String textSuggestions = textSuggestionsIsOn ? getString(R.string.yes) : getString(R.string.no);
+        String dictionaryName = (dictionaryType == 0) ? getString(R.string.basic) : getString(R.string.enhanced);
+        String orientation = (screenOrientation == 0) ? getString(R.string.vertical) : getString(R.string.horizontal);
         result = getCurrentResult();
         wpm = result.getWPM();
         String wpmText = new DecimalFormat("0.#").format(wpm) + " " + getString(R.string.wordsPerMinute) + "\n" + Emoji.getEmojiOfWpm((int) wpm);
         tvWPM.setText(wpmText);
-        tvIncorrectWords.setText(getString(R.string.incorrect_words) + ": " + (totalWords - correctWords));
-        tvCorrectWords.setText(getString(R.string.correct_words) + ": " + correctWords);
-        tvDictionary.setText(getString(R.string.dictionary) + ": " + DictionaryName);
-        tvAllottedTime.setText(getString(R.string.allotted_time) + ": " + TimeConvert.convertSeconds(this, timeInSeconds));
-        tvLanguage.setText(getString(R.string.selected_language) + ": " + dictionaryLanguage.getName());
-        tvTextSuggestions.setText(getString(R.string.text_suggestions) + ": " + textSuggestions);
+        tvIncorrectWords.setText(getString(R.string.incorrect_words_pl, (totalWords - correctWords)));
+        tvCorrectWords.setText(getString(R.string.correct_words_pl, correctWords));
+        tvDictionary.setText(getString(R.string.dictionary_pl, dictionaryName));
+        tvAllottedTime.setText(getString(R.string.selected_time_pl, TimeConvert.convertSeconds(this, timeInSeconds)));
+        tvLanguage.setText(getString(R.string.selected_language_pl, dictionaryLanguage.getName(this)));
+        tvTextSuggestions.setText(getString(R.string.text_suggestions_pl, textSuggestions));
+        tvScreenOrientation.setText(getString(R.string.screen_orientation_pl, orientation));
         if (wpm <= 0 && !calledFromResultsTab)
             Toast.makeText(this, getString(R.string.msg_results_with_zero_wpm), Toast.LENGTH_SHORT).show();
         else if (wpm>0 && !calledFromResultsTab) SaveResultData();
@@ -111,6 +110,7 @@ public class ResultActivity extends AppCompatActivity {
         timeInSeconds = arguments.getInt(StringKeys.TEST_AMOUNT_OF_SECONDS);
         totalWords = arguments.getInt(StringKeys.TOTAL_WORDS);
         dictionaryType = arguments.getInt(StringKeys.TEST_DICTIONARY_TYPE);
+        screenOrientation = arguments.getInt(StringKeys.TEST_SCREEN_ORIENTATION);
         dictionaryLanguage = (Language)arguments.getSerializable(StringKeys.TEST_DICTIONARY_LANG);
         textSuggestionsIsOn = arguments.getBoolean(StringKeys.TEST_SUGGESTIONS_ON);
         calledFromResultsTab = arguments.getBoolean(StringKeys.CALLED_FROM_PASSED_RESULTS);
@@ -128,12 +128,26 @@ public class ResultActivity extends AppCompatActivity {
             TextView tvNewBestResult = findViewById(R.id.tvNewBestResult);
             tvNewBestResult.setVisibility(View.VISIBLE);
         }
-
         currentUser.addResult(result);
-
+        checkAchievements();
         currentUser.storeData(this);
     }
 
+
+    public void checkAchievements() {
+        int newAchievements = 0;
+        for (Achievement achievement: currentUser.getAchievements()) {
+            if (achievement.getCompletionDate()==null && achievement.isCompleted(currentUser, result)) {
+                achievement.setCompletionDate(Calendar.getInstance().getTime());
+                newAchievements++;
+            }
+            if (newAchievements>0)
+                Toast.makeText(
+                        ResultActivity.this,
+                        getString(R.string.new_achievements_notification, newAchievements),
+                        Toast.LENGTH_LONG).show();
+        }
+    }
 
     public void SaveAndContinue(View view){
         finish();
@@ -149,6 +163,7 @@ public class ResultActivity extends AppCompatActivity {
                 correctWordsWeight,
                 totalWords,
                 dictionaryType,
+                screenOrientation,
                 dictionaryLanguage,
                 timeInSeconds,
                 textSuggestionsIsOn,
