e833b1f71a2f0a1d0571ce8e58980cbc076cfcae
TetTheDev <kirillgrayson@gmail.com>
2021-07-16 20:40:54 +0500

    Now initially language picks depending on system language or previously selected language.

--- app/src/main/java/com/example/typego/MainActivity.java
+++ app/src/main/java/com/example/typego/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.typego;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,13 +9,19 @@ import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
-
+import android.widget.TextView;
 import com.example.typego.utils.Language;
 import com.example.typego.utils.StringKeys;
 import com.example.typego.utils.User;
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final int DEFAULT_AMOUNT_OF_SECONDS = 60;
+    static final int DEFAULT_DICTIONARY_TYPE_INDEX = 0;
+    static final boolean DEFAULT_SUGGESTIONS_IS_ON = true;
+    static final int DEFAULT_SCREEN_ORIENTATION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +36,21 @@ public class MainActivity extends AppCompatActivity {
     public void startBasicTest(View v) {
         lockScreen();
         Intent intent = new Intent(this, TypingTestActivity.class);
-        Spinner spinner = findViewById(R.id.spinnerBasicTestLanguageSelection);
         Language selectedLanguage = (Language)spinner.getSelectedItem();
         intent.putExtra(StringKeys.TEST_DICTIONARY_LANG, selectedLanguage);
-        intent.putExtra(StringKeys.TEST_AMOUNT_OF_SECONDS, 60);
-        intent.putExtra(StringKeys.TEST_DICTIONARY_TYPE, 0);
-        intent.putExtra(StringKeys.TEST_SUGGESTIONS_ON, true);
-        intent.putExtra(StringKeys.TEST_SCREEN_ORIENTATION, 0);
+        intent.putExtra(StringKeys.TEST_AMOUNT_OF_SECONDS, DEFAULT_AMOUNT_OF_SECONDS);
+        intent.putExtra(StringKeys.TEST_DICTIONARY_TYPE, DEFAULT_DICTIONARY_TYPE_INDEX);
+        intent.putExtra(StringKeys.TEST_SUGGESTIONS_ON, DEFAULT_SUGGESTIONS_IS_ON);
+        intent.putExtra(StringKeys.TEST_SCREEN_ORIENTATION, DEFAULT_SCREEN_ORIENTATION);
         intent.putExtra(StringKeys.FROM_MAIN_MENU, true);
         startActivity(intent);
+        User user = User.getFromStoredData(this);
+        user.rememberUserPreferences(this,
+                selectedLanguage,
+                2,
+                DEFAULT_DICTIONARY_TYPE_INDEX,
+                DEFAULT_SUGGESTIONS_IS_ON,
+                DEFAULT_SCREEN_ORIENTATION);
         finish();
     }
 
@@ -86,12 +97,50 @@ public class MainActivity extends AppCompatActivity {
         currentUser.storeData(this);
     }
 
+    Spinner spinner;
     public void languageSpinnerInit() {
-        Spinner spinner = findViewById(R.id.spinnerBasicTestLanguageSelection);
+        spinner = findViewById(R.id.spinnerBasicTestLanguageSelection);
         SpinnerAdapter spinnerAdapter = new ArrayAdapter<>(
                 this,
                 R.layout.spinner_item,
                 Language.getAvailableLanguages(this));
         spinner.setAdapter(spinnerAdapter);
+        int preferredLanguageIndex = getPreferredLanguageIndex();
+        if (preferredLanguageIndex < 0)
+            preferredLanguageIndex = getSystemLanguageIndex();
+        spinner.setSelection(preferredLanguageIndex);
+    }
+
+    public int getPreferredLanguageIndex() {
+        ArrayList<Language> languages = Language.getAvailableLanguages(this);
+        User currentUser = User.getFromStoredData(this);
+        if (currentUser.getPreferredLanguage() == null) return -1;
+        String preferredLanguageId = currentUser.getPreferredLanguage().getIdentifier();
+        int index = 0;
+        for (Language language:languages) {
+            if (language.getIdentifier().equalsIgnoreCase(preferredLanguageId))
+                return index;
+            index++;
+        }
+        return 0;
+    }
+
+    public int getSystemLanguageIndex() {
+        ArrayList<Language> languages = Language.getAvailableLanguages(this);
+        String systemLanguage = Locale.getDefault().getDisplayLanguage().toLowerCase();
+        int index = 0;
+        for (Language language:languages
+             ) {
+            if (language.getName(MainActivity.this).equalsIgnoreCase(systemLanguage))
+                return index;
+            index++;
+        }
+        return 0;
+    }
+
+    public void resetUser(View view) {
+        User currentUser = new User();
+        currentUser.initAchievements(this);
+        currentUser.storeData(this);
     }
 }
