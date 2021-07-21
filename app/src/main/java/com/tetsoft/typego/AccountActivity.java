2df793828b37f6dffe6905ecd948ef55146b6b38
TetTheDev <kirillgrayson@gmail.com>
2021-07-21 21:42:45 +0500

    Package name change

--- app/src/main/java/com/tetsoft/typego/AccountActivity.java
+++ app/src/main/java/com/tetsoft/typego/AccountActivity.java
@@ -1,4 +1,4 @@
-package com.example.typego;
+package com.tetsoft.typego;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -6,7 +6,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import android.os.Bundle;
 import android.util.Log;
-import com.example.typego.utils.StringKeys;
+import com.tetsoft.typego.utils.StringKeys;
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
