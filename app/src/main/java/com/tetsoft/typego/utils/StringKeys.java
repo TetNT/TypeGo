252a7d85c26e35cdb04185433fc1bd74bbde1595
kiril <kirillgrayson@gmail.com>
2021-10-24 23:18:01 +0300

    New string keys added

--- app/src/main/java/com/tetsoft/typego/utils/StringKeys.java
+++ app/src/main/java/com/tetsoft/typego/utils/StringKeys.java
@@ -2,7 +2,6 @@ package com.tetsoft.typego.utils;
 
 public class StringKeys {
     // common strings
-
     public static final String USER_STORAGE_FILE = "user_storage";
     public static final String PREFERENCES_CURRENT_USER = "prefCurrentUser";
     public static final String TEST_AMOUNT_OF_SECONDS = "AmountOfSeconds";
@@ -14,10 +13,12 @@ public class StringKeys {
     public static final String TEST_SCREEN_ORIENTATION = "screenOrientation";
     public static final String TOTAL_WORDS = "totalWords";
     public static final String CALLED_FROM_PASSED_RESULTS = "calledFromPassedResults";
-    public static final String FRAGMENT_TEST_SETUP = "fragmentTestSetup";
-    public static final String FRAGMENT_ACCOUNT = "fragmentAccount";
     public static final String FROM_MAIN_MENU = "fromMainMenu";
     public static final String TEST_TYPED_WORDS_LIST = "typedWordsList";
+
+    public static final String FRAGMENT_TEST_SETUP = "fragmentTestSetup";
+    public static final String FRAGMENT_ACCOUNT = "fragmentAccount";
+    public static final String FRAGMENT_STATISTICS = "fragmentStatistics";
 
     // language identifiers
     public static final String LANGUAGE_ALL = "ALL";
@@ -28,4 +29,6 @@ public class StringKeys {
     public static final String LANGUAGE_ES = "ES";
     public static final String LANGUAGE_IT = "IT";
     public static final String LANGUAGE_KR = "KR";
+    public static final String LANGUAGE_UA = "UA";
+    public static final String LANGUAGE_BG = "BG";
 }
