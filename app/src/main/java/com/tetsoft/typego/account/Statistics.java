d0bde2286cdfc992a6497c6a210aee1af01b30e5
kiril <kirillgrayson@gmail.com>
2021-11-21 23:17:59 +0200

    Added comment

--- app/src/main/java/com/tetsoft/typego/account/Statistics.java
+++ app/src/main/java/com/tetsoft/typego/account/Statistics.java
@@ -81,6 +81,7 @@ public class Statistics {
         return count;
     }
 
+    // Returns the percentage of done achievements out of all achievements.
     public int getAchievementsProgressInPercents() {
         if (getDoneAchievementsCount() == 0 || user.getAchievements().size() == 0) return 0;
         return (getDoneAchievementsCount()*100)/user.getAchievements().size();
