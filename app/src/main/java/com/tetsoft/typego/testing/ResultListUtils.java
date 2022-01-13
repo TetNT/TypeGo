ab74935b62281386103e825255dce8efd3ba0120
kiril <kirillgrayson@gmail.com>
2022-01-13 22:28:19 +0200

    Added a method to get results list filtered by time mode

--- app/src/main/java/com/tetsoft/typego/testing/ResultListUtils.java
+++ app/src/main/java/com/tetsoft/typego/testing/ResultListUtils.java
@@ -2,6 +2,8 @@ package com.tetsoft.typego.testing;
 
 import androidx.annotation.NonNull;
 import com.tetsoft.typego.utils.Language;
+import com.tetsoft.typego.utils.TimeMode;
+
 import java.util.ArrayList;
 
 public final class ResultListUtils {
@@ -15,11 +17,20 @@ public final class ResultListUtils {
         return currentBest;
     }
 
-    public static ArrayList<TypingResult> getResultsByLanguage(@NonNull ArrayList<TypingResult> results, @NonNull Language language) {
+    public static ArrayList<TypingResult> getResultsByLanguage(ArrayList<TypingResult> results, Language language) {
         ArrayList<TypingResult> selectedResults = new ArrayList<>();
         for (TypingResult res: results) {
             if (res.getLanguage().getIdentifier().equalsIgnoreCase(language.getIdentifier()))
                 selectedResults.add(res);
+        }
+        return selectedResults;
+    }
+
+    public static ArrayList<TypingResult> getResultsByTimeMode(ArrayList<TypingResult> results, TimeMode timeMode) {
+        ArrayList<TypingResult> selectedResults = new ArrayList<>();
+        int comparingSeconds = timeMode.getTimeInSeconds();
+        for (TypingResult res: results) {
+            if (res.getTimeInSeconds() == comparingSeconds) selectedResults.add(res);
         }
         return selectedResults;
     }
