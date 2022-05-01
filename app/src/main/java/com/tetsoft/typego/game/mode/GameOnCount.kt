87d020388c69adda90d20ab653b08984b437363c
kiril <kirillgrayson@gmail.com>
2022-05-01 19:44:55 +0300

    New game modes

---
+++ app/src/main/java/com/tetsoft/typego/game/mode/GameOnCount.kt
@@ -0,0 +1,20 @@
+package com.tetsoft.typego.game.mode
+
+import com.tetsoft.typego.data.DictionaryType
+import com.tetsoft.typego.data.Language
+import com.tetsoft.typego.data.ScreenOrientation
+import com.tetsoft.typego.data.WordsCount
+import com.tetsoft.typego.data.language.LanguageSelectable
+
+class GameOnCount(
+    private val language: Language,
+    val wordsCount: WordsCount,
+    val dictionaryType: DictionaryType,
+    override val suggestionsActivated: Boolean,
+    override val screenOrientation: ScreenOrientation
+) : GameMode(suggestionsActivated, screenOrientation), LanguageSelectable {
+
+    override fun getLanguage(): Language {
+        return language
+    }
+}
---
+++ app/src/main/java/com/tetsoft/typego/game/mode/GameOnTime.kt
@@ -0,0 +1,29 @@
+package com.tetsoft.typego.game.mode
+
+import com.tetsoft.typego.data.DictionaryType
+import com.tetsoft.typego.data.Language
+import com.tetsoft.typego.data.ScreenOrientation
+import com.tetsoft.typego.data.TimeMode
+import com.tetsoft.typego.data.language.LanguageSelectable
+import java.io.Serializable
+
+class GameOnTime(
+    private val language: Language,
+    val timeMode: TimeMode,
+    val dictionaryType: DictionaryType,
+    suggestionsActivated: Boolean,
+    screenOrientation: ScreenOrientation
+) : GameMode(
+    suggestionsActivated,
+    screenOrientation
+), LanguageSelectable, Serializable {
+
+    fun calculateCpm(score: Int): Int {
+        return score
+    }
+
+    override fun getLanguage(): Language {
+        return language
+    }
+
+}
