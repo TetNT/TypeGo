b78cc276e4698a10ae66a72288779dc69aa75ab2
kiril <kirillgrayson@gmail.com>
2022-05-01 19:20:53 +0300

    Achievements unit test

--- app/src/test/java/com/tetsoft/typego/AchievementsListTest.java
+++ app/src/test/java/com/tetsoft/typego/AchievementsListTest.java
@@ -1,42 +1,24 @@
 package com.tetsoft.typego;
 
-import com.tetsoft.typego.data.achievement.Achievement;
-import com.tetsoft.typego.data.achievement.AchievementMigration;
-import com.tetsoft.typego.data.achievement.RequirementFactory;
+import com.tetsoft.typego.data.achievement.completion.AchievementsCompletionPair;
+import com.tetsoft.typego.data.achievement.completion.AchievementsProgressList;
 
 import org.junit.Test;
-import java.util.ArrayList;
+
 import static org.junit.Assert.*;
 
 public class AchievementsListTest {
     @Test
-    public void mergeIsCorrect() {
+    public void achievementsProgressListSizeIsCorrect() {
+        AchievementsProgressList achievementsProgressList = new AchievementsProgressList();
+        achievementsProgressList.add(new AchievementsCompletionPair(1, 1649778842000L));
+        achievementsProgressList.add(new AchievementsCompletionPair(2, 1649778882000L));
+        achievementsProgressList.add(new AchievementsCompletionPair(3, 0L));
+        achievementsProgressList.add(new AchievementsCompletionPair(4, 0L));
+        achievementsProgressList.add(new AchievementsCompletionPair(5, 0L));
+        achievementsProgressList.add(new AchievementsCompletionPair(6, 1649778842000L));
+        assertEquals(6, achievementsProgressList.size());
 
-        ArrayList<Achievement> oldAchievements = new ArrayList<>();
-        oldAchievements.add(new Achievement(0, "Prev", "Test", R.drawable.ic_achievement_star,
-                false, RequirementFactory.passedTestsAmount(1)));
-        oldAchievements.add(new Achievement(0, "Prev", "Test", R.drawable.ic_child,
-                true, RequirementFactory.WPMisMoreThan(20)));
-        oldAchievements.add(new Achievement(0, "Prev", "Test", R.drawable.ic_star,
-                true, RequirementFactory.WPMisMoreThan(40)));
-        oldAchievements.add(new Achievement(0, "Prev", "Test", R.drawable.ic_keyboard,
-                true, RequirementFactory.WPMisMoreThan(50)));
 
-        ArrayList<Achievement> newAchievements = new ArrayList<>();
-        newAchievements.add(new Achievement(1, "New", "Test", R.drawable.ic_achievement_star,
-                false, RequirementFactory.passedTestsAmount(1)));
-        newAchievements.add(new Achievement(3, "New", "Test", R.drawable.ic_star,
-                true, RequirementFactory.WPMisMoreThan(40)));
-        newAchievements.add(new Achievement(4, "New", "Test", R.drawable.ic_keyboard,
-                true, RequirementFactory.WPMisMoreThan(50)));
-        newAchievements.add(new Achievement(8, "New", "Test", R.drawable.ic_unmistakable,
-                false, RequirementFactory.timeModeNoMistakes(30)));
-        AchievementMigration migration = new AchievementMigration(oldAchievements, newAchievements);
-        ArrayList<Achievement> merged = migration.getMergedAchievementList();
-        assertEquals(merged.get(0).getName(), "Prev");
-        assertEquals(merged.get(1).getName(), "Prev");
-        assertEquals(merged.get(2).getName(), "Prev");
-        assertEquals(merged.get(3).getName(), "New");
-        assertEquals(merged.get(0).getId(), 1);
     }
 }
