e7a57836349e4b95a60195257cdbdea5470857e2
kiril <kirillgrayson@gmail.com>
2022-05-01 19:25:23 +0300

    Moved the keys to corresponding files.

--- app/src/main/java/com/tetsoft/typego/utils/StringKeys.kt
+++ app/src/main/java/com/tetsoft/typego/utils/StringKeys.kt
@@ -3,29 +3,18 @@ package com.tetsoft.typego.utils
 object StringKeys {
     // user's storage and preferences
     const val USER_STORAGE = "user_storage"
-    const val USER_PREFERENCES_STORAGE = "user_preferences_storage"
     const val USER_RESULTS_STORAGE = "user_results_storage"
-    const val ADS_COUNTER_STORAGE = "ads_counter_storage"
-    const val ADS_COUNTER_VALUE = "ads_counter_value"
-    const val USER_PREFERENCES = "userPreferences"
     const val PREFERENCE_LANGUAGE = "language"
     const val PREFERENCE_TIMEMODE = "timemode"
     const val PREFERENCE_DICTIONARY_TYPE = "dictionary_type"
     const val PREFERENCE_SUGGESTIONS = "suggestions"
     const val PREFERENCE_SCREEN_ORIENTATION = "screen_orientation"
-    const val USER_ACHIEVEMENTS = "userAchievements"
-    const val USER_RESULTS = "userResults"
     const val PREFERENCES_CURRENT_USER = "prefCurrentUser"
-    const val PREFERENCES_TEST_USER = "prefTestUser"
+    const val STORAGE_APP_VERSION = "stored_on_app_version"
 
     // test metadata
-    const val TEST_AMOUNT_OF_SECONDS = "AmountOfSeconds"
-    const val TEST_DICTIONARY_TYPE = "DictionaryType"
-    const val TEST_SUGGESTIONS_ON = "TextSuggestionsIsOn"
-    const val TEST_DICTIONARY_LANG = "DictionaryLanguageId"
     const val TEST_CORRECT_WORDS = "correctWords"
     const val TEST_CORRECT_WORDS_WEIGHT = "correctWordsWeight"
-    const val TEST_SCREEN_ORIENTATION = "screenOrientation"
     const val TOTAL_WORDS = "totalWords"
 
     // intent navigation extras
