1afbac38b7a2a15386ce6ab37bff74b113aa07b3
kiril <kirillgrayson@gmail.com>
2022-05-02 14:15:53 +0300

    Added language translation method

--- app/src/main/java/com/tetsoft/typego/data/Language.kt
+++ app/src/main/java/com/tetsoft/typego/data/Language.kt
@@ -1,10 +1,25 @@
 package com.tetsoft.typego.data
 
+import android.content.Context
+import android.content.res.Resources
 import java.io.Serializable
 
 data class Language(
     val identifier: String
 ) : Serializable {
+
+    // returns translated name from string resources. If resource is not found, returns identifier.
+    fun getName(context: Context): String {
+        return try {
+            context.getString(getResourceIdByName(context))
+        } catch (e: Resources.NotFoundException) {
+            identifier
+        }
+    }
+
+    private fun getResourceIdByName(context: Context): Int {
+        return context.resources.getIdentifier(identifier, "string", context.packageName)
+    }
 
     override fun toString(): String {
         return identifier
