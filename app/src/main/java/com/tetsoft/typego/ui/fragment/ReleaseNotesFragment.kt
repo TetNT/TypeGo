6b81573ad0b8760e529d59b658fdd43b21b448fb
kiril <kirillgrayson@gmail.com>
2022-05-02 19:14:22 +0300

    Converted to kotlin

--- app/src/main/java/com/tetsoft/typego/ui/fragment/ReleaseNotesFragment.kt
+++ app/src/main/java/com/tetsoft/typego/ui/fragment/ReleaseNotesFragment.kt
@@ -1,44 +1,27 @@
-package com.tetsoft.typego.ui.fragment;
+package com.tetsoft.typego.ui.fragment
 
-import android.content.Context;
-import android.os.Bundle;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.tetsoft.typego.R
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.tetsoft.typego.BuildConfig
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
+class ReleaseNotesFragment : Fragment() {
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-
-import com.tetsoft.typego.BuildConfig;
-import com.tetsoft.typego.R;
-
-import org.jetbrains.annotations.NotNull;
-
-public class ReleaseNotesFragment extends Fragment {
-
-
-    public ReleaseNotesFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_release_notes, container, false)
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_release_notes, container, false);
-    }
-
-    @Override
-    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
-        TextView tvNotesHeader = view.findViewById(R.id.tv_whats_new);
-        Context context = getContext();
-        if (context != null)
-            tvNotesHeader.setText(context.getString(R.string.whats_new_pl, BuildConfig.VERSION_NAME));
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val tvNotesHeader = view.findViewById<TextView>(R.id.tv_whats_new)
+        val context = context
+        if (context != null) tvNotesHeader.text =
+            context.getString(R.string.whats_new_pl, BuildConfig.VERSION_NAME)
     }
 }
