f819befd5b4c845814cf19b33248f82ef587d1eb
kiril <kirillgrayson@gmail.com>
2022-05-16 22:00:31 +0300

    Converted to kotlin

--- app/src/main/java/com/tetsoft/typego/data/achievement/requirement/RequirementFactory.kt
+++ app/src/main/java/com/tetsoft/typego/data/achievement/requirement/RequirementFactory.kt
@@ -1,56 +1,67 @@
-package com.tetsoft.typego.data.achievement.requirement;
+package com.tetsoft.typego.data.achievement.requirement
 
-import java.util.ArrayList;
+import java.util.ArrayList
 
-public class RequirementFactory {
-
-    public static ArrayList<Requirement> WPMisMoreThan(int moreOrEquals) {
-        Requirement requirement = new Requirement(
-                Requirement.AchievementSection.WPM,
-                Requirement.CompareType.MORE_OR_EQUALS,
-                moreOrEquals);
-        ArrayList<Requirement> requirements = new ArrayList<>();
-        requirements.add(requirement);
-        return requirements;
-
+object RequirementFactory {
+    fun wpmIsMoreThan(moreOrEquals: Int): ArrayList<Requirement> {
+        val requirement = Requirement(
+            Requirement.AchievementSection.WPM,
+            Requirement.CompareType.MORE_OR_EQUALS,
+            moreOrEquals
+        )
+        val requirements = ArrayList<Requirement>()
+        requirements.add(requirement)
+        return requirements
     }
 
-    public static ArrayList<Requirement> timeModeNoMistakes(int timeInSeconds) {
-        final int MINIMAL_WPM = 30;
-        ArrayList<Requirement> requirements = new ArrayList<>();
-        requirements.add(new Requirement(
+    // TODO: make it a parameter
+    private const val MINIMAL_WPM = 30
+    fun timeModeNoMistakes(timeInSeconds: Int): ArrayList<Requirement> {
+        val requirements = ArrayList<Requirement>()
+        requirements.add(
+            Requirement(
                 Requirement.AchievementSection.TIME_MODE,
                 Requirement.CompareType.EQUALS,
-                timeInSeconds));
-        requirements.add(new Requirement(
+                timeInSeconds
+            )
+        )
+        requirements.add(
+            Requirement(
                 Requirement.AchievementSection.MISTAKES,
                 Requirement.CompareType.EQUALS,
-                0));
-        requirements.add(new Requirement(
+                0
+            )
+        )
+        requirements.add(
+            Requirement(
                 Requirement.AchievementSection.WPM,
                 Requirement.CompareType.MORE_OR_EQUALS,
                 MINIMAL_WPM
-        ));
-        return requirements;
+            )
+        )
+        return requirements
     }
 
-
-    public static ArrayList<Requirement> passedTestsAmount(int amount) {
-        Requirement requirement = new Requirement(
-                Requirement.AchievementSection.PASSED_TESTS_AMOUNT,
-                Requirement.CompareType.MORE_OR_EQUALS,
-                amount);
-        ArrayList<Requirement> requirements = new ArrayList<>();
-        requirements.add(requirement);
-        return requirements;
+    fun passedTestsAmount(amount: Int): ArrayList<Requirement> {
+        val requirement = Requirement(
+            Requirement.AchievementSection.PASSED_TESTS_AMOUNT,
+            Requirement.CompareType.MORE_OR_EQUALS,
+            amount
+        )
+        val requirements = ArrayList<Requirement>()
+        requirements.add(requirement)
+        return requirements
     }
 
-    public static ArrayList<Requirement> differentLanguagesAmount(int amount) {
-        ArrayList<Requirement> requirements = new ArrayList<>();
-        requirements.add(new Requirement(
+    fun differentLanguagesAmount(amount: Int): ArrayList<Requirement> {
+        val requirements = ArrayList<Requirement>()
+        requirements.add(
+            Requirement(
                 Requirement.AchievementSection.DIFFERENT_LANGUAGES_COUNT,
                 Requirement.CompareType.MORE_OR_EQUALS,
-                amount));
-        return requirements;
+                amount
+            )
+        )
+        return requirements
     }
 }
--- app/src/main/java/com/tetsoft/typego/ui/fragment/AccountFragment.kt
+++ app/src/main/java/com/tetsoft/typego/ui/fragment/AccountFragment.kt
@@ -1,167 +1,159 @@
-package com.tetsoft.typego.ui.fragment;
+package com.tetsoft.typego.ui.fragment
 
-import android.content.Intent;
-import android.os.Bundle;
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
-import android.widget.AdapterView;
-import com.tetsoft.typego.TypeGoApp;
-import com.tetsoft.typego.adapter.GamesHistoryAdapter;
-import com.tetsoft.typego.adapter.language.LanguageSpinnerAdapter;
-import com.tetsoft.typego.adapter.language.LanguageSpinnerItem;
-import com.tetsoft.typego.data.language.LanguageList;
-import com.tetsoft.typego.game.mode.GameOnTime;
-import com.tetsoft.typego.game.result.GameResult;
-import com.tetsoft.typego.game.result.GameResultList;
-import com.tetsoft.typego.databinding.FragmentAccountBinding;
-import com.tetsoft.typego.storage.GameResultListStorage;
-import com.tetsoft.typego.ui.activity.ResultActivity;
-import com.tetsoft.typego.utils.AnimationManager;
-import com.tetsoft.typego.R;
-import com.tetsoft.typego.utils.StringKeys;
-import com.tetsoft.typego.data.language.Language;
-import java.util.ArrayList;
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationSet
+import android.widget.AdapterView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.tetsoft.typego.R
+import com.tetsoft.typego.TypeGoApp
+import com.tetsoft.typego.adapter.GamesHistoryAdapter
+import com.tetsoft.typego.adapter.GamesHistoryAdapter.RecyclerViewOnClickListener
+import com.tetsoft.typego.adapter.language.LanguageSpinnerAdapter
+import com.tetsoft.typego.adapter.language.LanguageSpinnerItem
+import com.tetsoft.typego.data.language.Language
+import com.tetsoft.typego.data.language.LanguageList
+import com.tetsoft.typego.databinding.FragmentAccountBinding
+import com.tetsoft.typego.game.mode.GameOnTime
+import com.tetsoft.typego.game.result.GameResultList
+import com.tetsoft.typego.storage.GameResultListStorage
+import com.tetsoft.typego.ui.activity.ResultActivity
+import com.tetsoft.typego.utils.AnimationManager
+import com.tetsoft.typego.utils.StringKeys
 
+class AccountFragment : Fragment() {
+    private var _binding: FragmentAccountBinding? = null
+    var selectedLanguage: Language? = null
+    private var resultList: GameResultList? = null
+    private var resultListStorage: GameResultListStorage? = null
+    private var inDescendingOrder = true
+    private val binding: FragmentAccountBinding
+        get() = _binding!!
 
-public class AccountFragment extends Fragment {
-
-    FragmentAccountBinding _binding;
-
-    Language selectedLanguage;
-
-    GameResultList resultList;
-
-    GameResultListStorage resultListStorage;
-
-    boolean inDescendingOrder = true;
-
-    @NonNull
-    private FragmentAccountBinding getBinding() {
-        return _binding;
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAccountBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    public AccountFragment() {  }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        _binding = FragmentAccountBinding.inflate(inflater, container, false);
-        return getBinding().getRoot();
-    }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        if (getActivity() != null) {
-            resultListStorage = ((TypeGoApp) (getActivity().getApplication())).getResultListStorage();
-            resultList = resultListStorage.get();
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (activity != null) {
+            resultListStorage = (requireActivity().application as TypeGoApp).resultListStorage
+            resultList = resultListStorage!!.get()
         }
-        initLanguageSpinner();
+        initLanguageSpinner()
     }
 
-    private void loadAccountData() {
-        setupStatFields();
-        updateResults();
-        loadLastResultsData();
+    private fun loadAccountData() {
+        setupStatFields()
+        updateResults()
+        loadLastResultsData()
     }
 
-    private void setupStatFields() {
-        getBinding().tvAverageWPM.setText(getString(R.string.average_wpm_pl, 0));
-        getBinding().tvBestResult.setText(getString(R.string.best_result_pl, 0));
-        getBinding().tvTestsPassed.setText(getString(R.string.tests_passed_pl, 0));
+    private fun setupStatFields() {
+        binding.tvAverageWPM.text = getString(R.string.average_wpm_pl, 0)
+        binding.tvBestResult.text = getString(R.string.best_result_pl, 0)
+        binding.tvTestsPassed.text = getString(R.string.tests_passed_pl, 0)
     }
 
-    private GameResultList getResults(Language language, boolean inDescendingOrder) {
-        GameResultList byLang = resultList.getResultsByLanguage(language);
-        if (inDescendingOrder) {
-            return byLang.getInDescendingOrder();
-        }
-        else return byLang;
+    private fun getResults(language: Language?, inDescendingOrder: Boolean): GameResultList {
+        val byLang = resultList!!.getResultsByLanguage(language!!)
+        return if (inDescendingOrder) {
+            byLang.inDescendingOrder
+        } else byLang
     }
 
-    public void initLanguageSpinner() {
-        ArrayList<LanguageSpinnerItem> languages = new LanguageList().getTranslatableListInAlphabeticalOrder(requireContext());
+    private fun initLanguageSpinner() {
+        val languages = LanguageList().getTranslatableListInAlphabeticalOrder(requireContext())
         // an option to show the whole stats
-        LanguageSpinnerItem spinnerItem = new LanguageSpinnerItem(
-                new Language(StringKeys.LANGUAGE_ALL),
-                requireContext().getString(R.string.ALL),
-                R.drawable.ic_language);
-        languages.add(0, spinnerItem);
-        LanguageSpinnerAdapter spinnerAdapter = new LanguageSpinnerAdapter(requireContext(), languages);
-        getBinding().spinnerResultsLanguageSelection.setAdapter(spinnerAdapter);
-        getBinding().spinnerResultsLanguageSelection.setSelection(0);
-        getBinding().spinnerResultsLanguageSelection.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                selectedLanguage = getBinding().spinnerResultsLanguageSelection.getSelectedLanguage();
-                loadAccountData();
-            }
+        val spinnerItem = LanguageSpinnerItem(
+            Language(StringKeys.LANGUAGE_ALL),
+            requireContext().getString(R.string.ALL),
+            R.drawable.ic_language
+        )
+        languages.add(0, spinnerItem)
+        val spinnerAdapter = LanguageSpinnerAdapter(requireContext(), languages)
+        binding.spinnerResultsLanguageSelection.adapter = spinnerAdapter
+        binding.spinnerResultsLanguageSelection.setSelection(0)
+//        val onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+//            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+//                TODO("Not yet implemented")
+//            }
+//
+//            override fun onNothingSelected(p0: AdapterView<*>?) {
+//                TODO("Not yet implemented")
+//            }
+//     }
+        binding.spinnerResultsLanguageSelection.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    selectedLanguage = binding.spinnerResultsLanguageSelection.getSelectedLanguage()
+                    loadAccountData()
+                }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
             }
-        });
     }
 
-    private void updateResults() {
-        GameResultList resultsByLanguage = getResults(selectedLanguage, inDescendingOrder);
+    private fun updateResults() {
+        val resultsByLanguage = getResults(selectedLanguage, inDescendingOrder)
         if (resultsByLanguage.isEmpty()) {
-            getBinding().tvPassedTestsInfo.setText(getString(R.string.msg_nothing_to_show));
-            return;
+            binding.tvPassedTestsInfo.text = getString(R.string.msg_nothing_to_show)
+            return
         }
-        getBinding().tvPassedTestsInfo.setText(getString(R.string.msg_passed_tests_information));
-        getBinding().tvTestsPassed.setText(getString(R.string.tests_passed_pl, resultsByLanguage.size()));
-        int bestResult = resultsByLanguage.getBestResultWpm();
-        getBinding().tvBestResult.setText(getString(R.string.best_result_pl, bestResult));
-        String wpmStr;
-        if (resultsByLanguage.size() >= 5) {
-            double wpmSum = 0;
-            for (GameResult res : resultsByLanguage) wpmSum += res.getWpm();
-            wpmStr = getString(R.string.average_wpm) + ": " + (int) (wpmSum / resultsByLanguage.size());
-            getBinding().tvAverageWPM.setText(wpmStr);
-        } else
-            getBinding().tvAverageWPM.setText(getString(R.string.msg_average_wpm_unavailable));
+        binding.tvPassedTestsInfo.text = getString(R.string.msg_passed_tests_information)
+        binding.tvTestsPassed.text = getString(R.string.tests_passed_pl, resultsByLanguage.size)
+        val bestResult = resultsByLanguage.bestResultWpm
+        binding.tvBestResult.text = getString(R.string.best_result_pl, bestResult)
+        val wpmStr: String
+        if (resultsByLanguage.size >= 5) {
+            var wpmSum = 0.0
+            for (res in resultsByLanguage) wpmSum += res!!.wpm
+            wpmStr =
+                getString(R.string.average_wpm) + ": " + (wpmSum / resultsByLanguage.size).toInt()
+            binding.tvAverageWPM.text = wpmStr
+        } else binding.tvAverageWPM.text = getString(R.string.msg_average_wpm_unavailable)
     }
 
-    public void loadLastResultsData() {
-        GameResultList resultsByLanguage = getResults(selectedLanguage, inDescendingOrder);
-        GamesHistoryAdapter.RecyclerViewOnClickListener listener = (v, position) -> {
-            GameResult resultInfo = resultsByLanguage.get(position);
-            Intent intent = new Intent(getContext(), ResultActivity.class);
-            intent.putExtra(StringKeys.TEST_CORRECT_WORDS, resultInfo.getCorrectWords());
-            intent.putExtra(StringKeys.TEST_CORRECT_WORDS_WEIGHT, resultInfo.getScore());
-            intent.putExtra(StringKeys.TOTAL_WORDS, resultInfo.getWordsWritten());
-            intent.putExtra(StringKeys.TEST_SETTINGS, (GameOnTime) resultInfo.getGameMode());
-            intent.putExtra(StringKeys.CALLED_FROM_PASSED_RESULTS, true);
-            startActivity(intent);
-        };
-        getBinding().rvPassedTests.setAdapter(new GamesHistoryAdapter(getContext(), resultsByLanguage, listener));
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
-        getBinding().rvPassedTests.setLayoutManager(linearLayoutManager);
-        AnimationManager animationManager = new AnimationManager();
-        Animation slideIn = animationManager.getSlideInAnimation(0, 50f, 500);
-        Animation fadeIn = animationManager.getFadeInAnimation(500);
-        AnimationSet animationSet = new AnimationSet(false);
-        animationSet.addAnimation(slideIn);
-        animationSet.addAnimation(fadeIn);
-        getBinding().rvPassedTests.setAnimation(animationSet);
+    private fun loadLastResultsData() {
+        val resultsByLanguage = getResults(selectedLanguage, inDescendingOrder)
+        val listener = RecyclerViewOnClickListener { _: View?, position: Int ->
+            val resultInfo = resultsByLanguage[position]
+            val intent = Intent(context, ResultActivity::class.java)
+            intent.putExtra(StringKeys.TEST_CORRECT_WORDS, resultInfo!!.correctWords)
+            intent.putExtra(StringKeys.TEST_CORRECT_WORDS_WEIGHT, resultInfo.score)
+            intent.putExtra(StringKeys.TOTAL_WORDS, resultInfo.wordsWritten)
+            intent.putExtra(StringKeys.TEST_SETTINGS, resultInfo.gameMode as GameOnTime)
+            intent.putExtra(StringKeys.CALLED_FROM_PASSED_RESULTS, true)
+            startActivity(intent)
+        }
+        binding.rvPassedTests.adapter = GamesHistoryAdapter(context, resultsByLanguage, listener)
+        val linearLayoutManager = LinearLayoutManager(context)
+        binding.rvPassedTests.layoutManager = linearLayoutManager
+        val animationManager = AnimationManager()
+        val slideIn: Animation = animationManager.getSlideInAnimation(0f, 50f, 500)
+        val fadeIn: Animation = animationManager.getFadeInAnimation(500)
+        val animationSet = AnimationSet(false)
+        animationSet.addAnimation(slideIn)
+        animationSet.addAnimation(fadeIn)
+        binding.rvPassedTests.animation = animationSet
     }
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        _binding = null;
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
