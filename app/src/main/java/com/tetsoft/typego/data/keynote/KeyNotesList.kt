75cf3b6af8bef6b98d8406fe9e74190dba98a050
kiril <kirillgrayson@gmail.com>
2023-12-22 15:13:43 +0200

    Added new achievements, updated icons, fixed caching, added a key note

--- app/src/main/java/com/tetsoft/typego/data/keynote/KeyNotesList.kt
+++ app/src/main/java/com/tetsoft/typego/data/keynote/KeyNotesList.kt
@@ -11,6 +11,7 @@ interface KeyNotesList {
             return listOf(
                 KeyNote("redesign_153", R.drawable.ic_achievement_alien, R.string.key_note_redesign, R.string.key_note_redesign_description),
                 KeyNote("seeds_introduction_153", R.drawable.ic_key, R.string.key_note_seeds, R.string.key_note_seeds_description),
+                KeyNote("new_achievements_153", R.drawable.ic_trophy, R.string.key_note_achievements, R.string.key_note_achievements_description),
             )
         }
     }
