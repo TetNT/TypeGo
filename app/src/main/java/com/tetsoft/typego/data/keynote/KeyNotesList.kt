f5ca7619372435b08b4ba268e3bda07c7e1f377d
corec <kirillgrayson@gmail.com>
2024-08-27 23:15:54 +0300

    Key notes added

--- app/src/main/java/com/tetsoft/typego/data/keynote/KeyNotesList.kt
+++ app/src/main/java/com/tetsoft/typego/data/keynote/KeyNotesList.kt
@@ -1,5 +1,6 @@
 package com.tetsoft.typego.data.keynote
 
+import com.tetsoft.typego.R
 import javax.inject.Inject
 
 interface KeyNotesList {
@@ -8,8 +9,8 @@ interface KeyNotesList {
     class Standard @Inject constructor() : KeyNotesList {
         override fun get(): List<KeyNote> {
             return listOf(
-                //KeyNote("new_game_mode_160", TODO(), R.string.key_note_new_game_mode, R.string.key_note_new_game_mode_description),
-                //KeyNote("new_theme_160", TODO(), R.string.key_note_new_theme, R.string.key_note_new_theme_description),
+                KeyNote("new_game_mode_160", R.drawable.text_filled, R.string.key_note_new_game_mode, R.string.key_note_new_game_mode_description),
+                KeyNote("new_theme_160", R.drawable.button_lilac, R.string.key_note_new_theme, R.string.key_note_new_theme_description),
             )
         }
     }
